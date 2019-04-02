0a59e84aea6cae5624d662fe082a0fdcc442a982
kevin Lam <kevin07696@gmail.com>
2019-04-02 14:32:29 -0400

    removed db

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var app = express();
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  });
-
 // set port
 var port = process.env.PORT || 8080
 app.use(express.static(__dirname));
@@ -15,19 +9,6 @@ app.use(express.static(__dirname));
 app.get("/", function(req, res) {
     res.render("index");
 })
-
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 
 app.listen(port, function() {
     console.log("app running")
